3c3da964fbc142fd83bd6c2f775c027ddd1578c2
calonsogarcia <carolina.alon.garcia@gmail.com>
2021-11-21 21:53:14 +0100

    Some styling done, still fighting!

--- src/components/Charity/CharitiesPage.jsx
+++ src/components/Charity/CharitiesPage.jsx
@@ -1,21 +1,19 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import charityService from "../../services/charity-service";
-//import SearchBar from "../Search/SearchBar";
+import {Container} from '@material-ui/core'
+
 
 class CharitiesPage extends Component {
   state = {
     listOfCharities: null,
     isLoading: true,
-   // showSearchBar: false,
   };
 
-  // handleToggle = () => {
-  //   this.setState({ showSearchBar: !this.state.showSearchBar });
-  // };
+ 
 
   componentDidMount() {
-    // console.log(process.env.REACT_APP_SERVER_API);
+
     charityService
       .getCharities()
       .then((response) => {
@@ -30,17 +28,11 @@ class CharitiesPage extends Component {
     const { listOfCharities, isLoading } = this.state;
 
     return (
-      <div>
+      <Container>
         <h1>All The Charities</h1>
 
         {isLoading && <h1>...isLoading</h1>}
 
-        {/* <button 
-        onClick={this.handleToggle}>
-        {this.state.showSearchBar ? "Hide Search" : "Search"}
-        </button>
-
-        {this.state.showSearchBar && <SearchBar />} */}
        
         {!isLoading &&
           listOfCharities.map((eachCharity) => {
@@ -53,7 +45,7 @@ class CharitiesPage extends Component {
               </div>
             );
           })}
-      </div>
+      </Container>
     );
   }
 }
