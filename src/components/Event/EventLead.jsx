e6c38a0680fe46bc90fbbd2e149d73db596092c3
louise <louise.birchall@gmx.com>
2021-11-22 19:56:27 +0100

    cheese rolling sorted

--- src/components/Event/EventLead.jsx
+++ src/components/Event/EventLead.jsx
@@ -6,31 +6,67 @@ import { Typography } from "@material-ui/core";
 import { Card } from "@material-ui/core";
 import { CardMedia } from "@material-ui/core";
 
+const styles = {
+  divStyle: {
+    display: "flex",
+    flexDirection: "Row",
+  },
+};
+
 class EventLead extends Component {
 
   render() {
     return (
-      <div>
+      <div style={styles.divStyle}>
         <div>
           <Card sx={{ maxWidth: 345 }}>
             <CardMedia
               component="img"
               height="250"
-              image={cheeseRolling}
-              alt="cheese rolling"
+              image={bookBitch}
+              alt="book bitch"
             />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
-                Cheese Rolling
+                Book Bitch
               </Typography>
               <Typography variant="body2" color="text.secondary">
-                People chase a wheel of Double Gloucester down a very steep hill
-                in Gloucestershire. The cheese has a one-second head start, and
-                the first one down wins the cheese
+                This time we were reading the Davinci Code. We hated it, it was
+                almost as bad as Twilight. We all got together and had a great
+                time talking about how terrible the plot holes were. Sharon
+                brought some lovely scones, too. I recommend this event to
+                anyone with actual taste.
               </Typography>
             </CardContent>
           </Card>
-          <h2>Placeholder Event Review</h2>
+        </div>
+        <div>
+          <Card>
+            <CardContent>
+              <Typography
+                sx={{ fontSize: 14 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                Events
+              </Typography>
+              <Typography variant="body2">
+                If you want to find other like-minded people to enjoy your
+                hobbies with you can find then via events, or even set up your
+                own! What are you waiting for?
+              </Typography>
+              <Link to="/events">See all the Events</Link>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default EventLead;
+
+{/* <h2>Placeholder Event Review</h2>
           <h3>BOOK BITCH</h3>
           <img src={bookBitch} alt="bookBitch" />
           <p>
@@ -56,11 +92,4 @@ class EventLead extends Component {
               are you waiting for?
             </Typography>
             <Link to="/events">See all the Events</Link>
-          </CardContent>
-        </div>
-      </div>
-    );
-  }
-}
-
-export default EventLead;
+          </CardContent> */}
