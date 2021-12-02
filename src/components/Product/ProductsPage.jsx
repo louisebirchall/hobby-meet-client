a303d3a32518b530926ef3d79317e5b1b6f7ece4
calonsogarcia <carolina.alon.garcia@gmail.com>
2021-12-02 11:01:32 +0100

    Product foto showing, cahnge buttons order

--- src/components/Product/ProductsPage.jsx
+++ src/components/Product/ProductsPage.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import productService from "../../services/product-service";
-import {Container, Button, Card, CardMedia, CardContent, Typography, Grid, CardActions} from '@material-ui/core'
-import Payment from "../Payment/Payment"
+import {
+  Container,
+  Button,
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  Grid,
+  CardActions,
+} from "@material-ui/core";
+import Payment from "../Payment/Payment";
 import { PuffLoader } from "react-spinners";
-
-
 
 class ProductsPage extends Component {
   state = {
@@ -14,13 +21,13 @@ class ProductsPage extends Component {
     itemToBuy: null,
   };
 
-   handleClick = (item) => {
-    this.setState({itemToBuy: item})
-  }
+  handleClick = (item) => {
+    this.setState({ itemToBuy: item });
+  };
 
   componentDidMount() {
     productService
-    .getProducts()
+      .getProducts()
       .then((response) => {
         this.setState({ listOfProducts: response.data, isLoading: false });
       })
@@ -29,15 +36,20 @@ class ProductsPage extends Component {
       });
   }
 
-
   render() {
     const { listOfProducts, isLoading, itemToBuy } = this.state;
 
     return (
       <Container style={{ paddingBottom: 60 }}>
-        <div align="center" style={{marginBottom: 10}}><Typography variant="h2">All The Products</Typography></div>
-        <Grid container spacing={3} style={{ display: "flex", justifyContent: "center" }}>
-          {isLoading && <PuffLoader  size="100px" color="orchid" />}
+        <div align="center" style={{ marginBottom: 10 }}>
+          <Typography variant="h2">All The Products</Typography>
+        </div>
+        <Grid
+          container
+          spacing={3}
+          style={{ display: "flex", justifyContent: "center" }}
+        >
+          {isLoading && <PuffLoader size="100px" color="orchid" />}
 
           {!isLoading &&
             listOfProducts.map((eachProduct) => {
@@ -46,36 +58,60 @@ class ProductsPage extends Component {
                   <Card xs={12} md={6} lg={4}>
                     <CardMedia align="center">
                       {eachProduct.image && (
-                        <img src={eachProduct.image}  alt={eachProduct.title} height="150px"/>)}
+                        <img
+                          src={eachProduct.image}
+                          alt={eachProduct.title}
+                          height="150px"
+                        />
+                      )}
                     </CardMedia>
-                    <CardContent sx={{ flex: '1 0 auto' }}>
-                      <Typography component="div" variant="h5">{eachProduct.title}</Typography>
-                      <Typography  component="div" variant="p">{eachProduct.price} €</Typography>
+                    <CardContent sx={{ flex: "1 0 auto" }}>
+                      <Typography component="div" variant="h5">
+                        {eachProduct.title}
+                      </Typography>
+                      <Typography component="div" variant="p">
+                        {eachProduct.price} €
+                      </Typography>
                     </CardContent>
                     <CardActions>
-                      <Button color="primary"
-                  variant="contained" onClick={() => this.handleClick(eachProduct)} href={'/products/payments/create-payment-intent'}>
-                  <Typography component="div" variant="h5">
-                  Buy it!</Typography></Button>
-                      {itemToBuy && itemToBuy._id === eachProduct._id && <Payment itemToBuy={itemToBuy}/>}
-
-                      <Button 
-                      variant="outlined" 
-                      href={`/products/${eachProduct._id}`}>
-                      <Typography component="div" variant="h5">
-                      details!</Typography>
+                      <Button
+                        variant="outlined"
+                        href={`/products/${eachProduct._id}`}
+                      >
+                        <Typography component="div" variant="h5">
+                          details!
+                        </Typography>
                       </Button>
-
+                      <Button
+                        color="primary"
+                        variant="contained"
+                        onClick={() => this.handleClick(eachProduct)}
+                        href={"/products/payments/create-payment-intent"}
+                      >
+                        <Typography component="div" variant="h5">
+                          Buy it!
+                        </Typography>
+                      </Button>
+                      {itemToBuy && itemToBuy._id === eachProduct._id && (
+                        <Payment itemToBuy={itemToBuy} />
+                      )}
                     </CardActions>
                   </Card>
-                  
                 </Grid>
-                
               );
             })}
         </Grid>
-        <div align="center" style={{marginTop: 10}}><Button style={{marginTop: 20}} color="secondary" variant="contained" component={Link} to="/products/create">Add Product!</Button></div>
-
+        <div align="center" style={{ marginTop: 10 }}>
+          <Button
+            style={{ marginTop: 20 }}
+            color="secondary"
+            variant="contained"
+            component={Link}
+            to="/products/create"
+          >
+            Add Product!
+          </Button>
+        </div>
       </Container>
     );
   }
