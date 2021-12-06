9d0abd960ae1d0ea3c1d2f45939ab576f4d7822a
Florian Becker <becker_florian@online.de>
2021-12-06 17:17:26 +0100

    different changes; profile, leaflet, styles

--- src/components/Event/EventDetails.jsx
+++ src/components/Event/EventDetails.jsx
@@ -17,6 +17,7 @@ import {
 import Payment from "../Payment/Payment";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { PuffLoader } from "react-spinners";
+// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 class EventDetails extends Component {
   state = {
@@ -102,6 +103,23 @@ class EventDetails extends Component {
                 <Typography>Description: {singleEvent.description} </Typography>
 
                 <Typography>Where: {singleEvent.location} </Typography>
+
+                {/* <MapContainer
+                  center={[51.505, -0.09]}
+                  zoom={13}
+                  scrollWheelZoom={false}
+                >
+                  <TileLayer
+                    
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  />
+                  <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                      A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                  </Marker>
+                </MapContainer> */}
 
                 <Typography>
                   Date: {formattedDate.toLocaleDateString()}
--- src/components/User/Profile.jsx
+++ src/components/User/Profile.jsx
@@ -40,53 +40,74 @@ class Profile extends Component {
     return (
       <Container style={{ paddingBottom: 60 }}>
         <div align="center" style={{ marginBottom: 10 }}>
-          <Typography variant="h1">Welcome</Typography>
+          <Typography variant="h1">Welcome </Typography>
         </div>
+        
+
         <Card sx={{ display: "flex" }}>
           {isLoading && <PuffLoader size="100px" color="orchid" />}
 
           {!isLoading && (
-            <Box sx={{ display: "flex", flexDirection: "row" }}>
+            <Box sx={{ display: "flex", flexDirection: "column"}}>
               <CardContent sx={{ flex: "1 0 auto" }}>
-                
+              {/* <Typography variant="h1">Welcome, {user.username} </Typography> */}
+
                 <CardMedia
-                align="center"
+                // align="center"
                   component="img"
                   image={user.image}
                   alt={user.username}
-                  style={{ borderRadius: 50, width: 250 }}
+                  style={{ 
+                  borderRadius: "50%", 
+                  borderStyle: "solid", 
+                  borderWidth: "thin",
+                  borderColor: "#3AEFD5", 
+                  width: 250 
+                  }}
                 />
+                {/* <hr/> */}
+                   <div >
+                    {/* <Typography component="div" variant="h2">Information</Typography> */}
+                    <Typography component="div" variant="h3">
+                      {user.username}
+                    </Typography>
+                    <Typography variant="p" color="text.secondary" component="div">
+                      {user.email}
+                    </Typography>
+                    <Typography variant="p" color="text.secondary" component="div">
+                      Name - {user.fullName}
+                    </Typography>
+                    <Typography variant="p" color="text.secondary" component="div">
+                      Attending - {user.events}
+                    </Typography>
+                    <Typography variant="p" color="text.secondary" component="div">
+                      Age - {user.age}
+                    </Typography>
+                    <Typography variant="p" color="text.secondary" component="div">
+                      User type - {user.type}
+                    </Typography>
+                    <Typography variant="p" color="text.secondary" component="div">
+                      Gender - {user.sex}
+                    </Typography>
+                    </div>
+                      {/* <div>
+                  <Typography component="div" variant="h2">Hobbies & Events</Typography>
 
-                <Typography component="div" variant="h3">
-                  {user.username}
-                </Typography>
-                <Typography variant="p" color="text.secondary" component="div">
-                  {user.email}
-                </Typography>
-                <Typography variant="p" color="text.secondary" component="div">
-                  Name - {user.fullName}
-                </Typography>
-                {/* <Typography variant="p" color="text.secondary" component="div">Hobbies - {user.hobbies}</Typography> */}
-                <Typography variant="p" color="text.secondary" component="div">
-                  Attending - {user.events}
-                </Typography>
-                <Typography variant="p" color="text.secondary" component="div">
-                  Age - {user.age}
-                </Typography>
-                <Typography variant="p" color="text.secondary" component="div">
-                  User type - {user.type}
-                </Typography>
-                <Typography variant="p" color="text.secondary" component="div">
-                  Gender - {user.sex}
-                </Typography>
+                  
+                  <Typography variant="p" color="text.secondary" component="div">Hobbies - {user.hobbies}</Typography>
+                  </div> */}
+                  {/* </div> */}
+
+                 <div align="center" style={{ marginTop: 30}}>
                 <Button
                   color="primary"
                   variant="contained"
                   component={NavLink}
                   to={`/users/${user._id}/edit`}
                 >
-                  Edit
+                  Edit Profile
                 </Button>
+                </div>
               </CardContent>
             </Box>
           )}
